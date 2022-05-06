44f4188b3f3825d9f2cf85d10aff15360de26a14
AYOUB-EL <52068209+bbouya@users.noreply.github.com>
2022-05-06 16:00:33 +0000

    Update environmental_station_sumulation.py

--- Virtual_stations/environmental_station_sumulation.py
+++ Virtual_stations/environmental_station_sumulation.py
@@ -1,38 +1,41 @@
-# Importation de library 
-
-from flask import stream_with_context
 from AWSIoTPythonSDK.MQTTLib import AWSIoTMQTTClient
 from random import randint
 import math
 import sys
 import logging
 import time
-import json 
+import json
 import datetime
 import argparse
 import boto3
-import parser   
+import parser
 
-dynamodb = boto3.resource('dynamodb', region_name = 'YOUR_REGION')  # connection to DynamoDB and access
-dynamoTable = dynamodb.Table('YOUR_TABLE_NAME')                     # To the table environmentalstation that will store the data provided
-jsonP = ''                                                          # by the two simulated stations
+dynamodb = boto3.resource('dynamodb', region_name='YOUR_REGION')                    # connection to DynamoDB and access
+dynamoTable = dynamodb.Table('YOUR_TABLE_NAME')                                     # to the table EnvironmentalStation that will store the data provided
+jsonP = '';                                                                         # by the two simulated stations
 
-def data_store(client, userdata , msg):                             # The function publishes the recieved data
+def data_store(client, userdata, msg):                                              # the function stores the recieved data in the DynamoDB table
     payload = str(msg.payload)[2:-1]
     jsonP = json.loads(payload)
     dynamoTable.put_item(Item=jsonP)
 
-def random_values():                                                # The function provides in a simple way random environmental values ....
-    temperature = str(randint(-50, 50))
+
+def send_data(myClient, data, topic):                                               # the function publishes the recieved data
+    messageJson = json.dumps(data)
+    myClient.publish(topic, messageJson, 1)
+    print("########## DATA RECIEVED ##########")
+    print("Published on topic: %s\nData provided by: %s\nRecieved data:\n%s\n" % (topic, clientId, messageJson))
+    print("I'm sending the data to DynamoDB... \n \n")
+
+def random_values():                                                                 # the function provides in a simple way random environmental values...
+    temperature = str(randint(-50,50))
     humidity = str(randint(0, 100))
     wind_direction = str(randint(0, 360))
     wind_intensity = str(randint(0, 100))
     rain_height = str(randint(0, 50))
-    datatime = str(datetime.datetime.now())[:19]                     # ... and take notice about the time of the detection, that is important for data storing.
-    
-    return temperature, humidity, wind_direction, wind_intensity, rain_height, datetime
+    datatime = str(datetime.datetime.now())[:19]                                     #... and take notice about the time of the detection, that is important for data storing in the table
 
-
+    return temperature, humidity, wind_direction, wind_intensity, rain_height, datatime
 
 def awsconnection(useWebsocket = False,                                          # the function sets the connection to AWS
     clientId = "",                                                               # client id
@@ -81,11 +84,29 @@ def awsconnection(useWebsocket = False,                                         
 
     return myClient
 
-# Arguments passing : in this case the only argument that we need is the clientID, that is supposed to be station1 or station2
-parser = argparse.ArguementParser()
-parser.add_arguement('--clientis', type = str)
+
+# Arguments passing: in this case the only argument that we need is the clientId, that is supposed to be station1 or station2
+parser = argparse.ArgumentParser()
+parser.add_argument('--clientid', type=str)
 args = parser.parse_args()
 clientId = args.clientid
-topic = 'YOUR TOPICS'
+topic = "YOUR_TOPIC"
 
-myClient = awsconnection(clientId=clientId)
+
+myClient =  awsconnection(clientId=clientId)
+myClient.connect()
+myClient.subscribe(topic, 1, data_store)
+
+# Every 5 seconds, random data are generated, sent and published
+while True:
+
+    temperature, humidity, wind_direction, wind_intensity, rain_height, datatime = random_values()
+
+    data = {"ID":clientId, "datetime":datatime, "Temperature":temperature, "Humidity":humidity,
+           "WindDirection":wind_direction, "WindIntensity":wind_intensity, "RainHeight":rain_height}
+
+    send_data(myClient, data, topic)
+
+    time.sleep(5)
+
+myClient.disconnect()
