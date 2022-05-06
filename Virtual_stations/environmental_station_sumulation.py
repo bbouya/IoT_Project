44bbbdb5a35ac8382276030a318321bc41112c4a
AYOUB-EL <52068209+bbouya@users.noreply.github.com>
2022-05-06 15:53:20 +0000

    Update environmental_station_sumulation.py

--- Virtual_stations/environmental_station_sumulation.py
+++ Virtual_stations/environmental_station_sumulation.py
@@ -1,5 +1,6 @@
 # Importation de library 
 
+from flask import stream_with_context
 from AWSIoTPythonSDK.MQTTLib import AWSIoTMQTTClient
 from random import randint
 import math
@@ -31,10 +32,53 @@ def random_values():                                                # The functi
     
     return temperature, humidity, wind_direction, wind_intensity, rain_height, datetime
 
-def awsconnection(useWebsocket = False,
-             clientId = '', 
-             thingName = 'your_thing_name', 
-             host = 'your_endpoint',
-             capath = 'your_path/rootCa.pem', 
-             certPath = 'your_path/xxxxx-certification.pem.crt', 
-             keyPath = 'your_path/xxxxx-private.pem.key'):
+
+
+def awsconnection(useWebsocket = False,                                          # the function sets the connection to AWS
+    clientId = "",                                                               # client id
+    thingName = "YOUR_THING_NAME",                                               # thing name (on AWS)
+    host = "YOUR_ENDPOINT",                                                      # your AWS endpoint
+    caPath = "YOUR_PATH/rootCa.pem",                                             # rootCA certificate (folder's path)
+    certPath = "YOUR_PATH/XXXXXXXXXX-certificate.pem.crt",                       # client certificate (folder's path)
+    keyPath = "YOUR_PATH/XXXXXXXXXX-private.pem.key"                             # private key (folder's path)
+    ):
+
+    port = 8883 if not useWebsocket else 443
+    useWebsocket = useWebsocket
+    clientId = clientId
+    host = host
+    port = port
+    rootCaPath = caPath
+    privateKeyPath = keyPath
+    certificatePath = certPath
+
+    # Logger settings
+    # more information at https://docs.python.org/3/library/logging.html
+    logger = logging.getLogger("AWSIoTPythonSDK.core")
+    logger.setLevel(logging.NOTSET)                                                      # NOTSET causes all messages to be processed when the logger is the root logger
+    streamHandler = logging.StreamHandler()                                              # sends logging output to streams
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    streamHandler.setFormatter(formatter)
+    logger.addHandler(streamHandler)
+
+    # AWSIoTMQTTClient initialization
+    myClient = None
+    if useWebsocket:
+        myClient = AWSIoTMQTTClient(clientId, useWebsocket=True)
+        myClient.configureEndpoint(host, port)
+        myClient.configureCredentials(rootCaPath)
+    else:
+        myClient = AWSIoTMQTTClient(clientId)
+        myClient.configureEndpoint(host, port)
+        myClient.configureCredentials(rootCaPath, privateKeyPath, certificatePath)
+
+    # AWSIoTMQTTClient connection configuration
+    myClient.configureAutoReconnectBackoffTime(1, 32, 20)
+    myClient.configureOfflinePublishQueueing(-1)      # param: if set to 0, the queue is disabled. If set to -1, the queue size is set to be infinite.
+    myClient.configureDrainingFrequency(2)            # Draining: 2 Hz
+    myClient.configureConnectDisconnectTimeout(10)    # 10 sec
+    myClient.configureMQTTOperationTimeout(5)         # 5 sec
+
+    return myClient
+
+# Arguments passing : in this case the only argument that we need is the clientID
